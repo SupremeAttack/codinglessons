3868afba078279bb926726c0eb62923a8f6ff8db
Alexandre Codjovi <alexandre.codjovi@sap.com>
2020-02-17 15:40:29 +0100

    first glance; variables, types; print; loops

---
+++ s1_intro.py
@@ -0,0 +1,78 @@
+myAge = 26
+myAgeInTenYears = 26 + 10
+#Operations
+# Display on screen$
+print ("covee is bored")
+print ("my current age: " + str(myAge) + " and my age in ten years: " + str(myAgeInTenYears))
+
+#Math Ops
+
+result = 10 + 10
+result = 10 / 10
+result = 10 * 10
+result = 10 % 10
+result = 10 - 10
+print (result) # 0
+
+result = result + 1
+
+print (result) # 1
+
+tenTimesResult = result * 10
+
+print (result) # 10
+
+result = "alex is great"
+
+print (result) # alex is great
+
+
+result = "alex " + " loves " + "covee"
+
+
+print (result) # alex loves covee
+
+
+# print all numbers 1 -> 10
+
+#12345678910
+
+result = ""
+counter = 1
+while(counter <= 10):
+#START
+  result = result + " " + str(counter) 
+  counter = counter + 1
+#END
+
+print ("all the numbers to ten:" + result) # all the numbers to ten: 1 2 3 4 5 6 7 8 9 10
+
+print (result) # 1 2 3 4 5 6 7 8 9 10
+  
+#  content of result variable
+#1 - 1
+#2 - 12
+#3 - 123
+#4 - 1234
+# ...
+#10 - 12345678910
+#11
+
+## GET THE SUM OF ALL NUMBERS TO TEN
+finalSum = 0
+counter = 1
+while(counter <= 3):
+#START
+  finalSum = finalSum + counter
+  counter = counter + 1
+#END
+
+print (finalSum) #6
+
+
+
+
+
+## JAVA or C++
+# Integer age = 10;
+# String  name = "ALEX";
